cd46aee52483e5ac7b192b7133299e7ec34d2820
Hythan <hythan_mco@hotmail.com>
2019-05-29 20:09:11 -0300

    Criado projeto

---
+++ src/main/java/br/edu/utfpr/atividade2/Atividade2Application.java
@@ -0,0 +1,13 @@
+package br.edu.utfpr.atividade2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Atividade2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Atividade2Application.class, args);
+	}
+
+}
