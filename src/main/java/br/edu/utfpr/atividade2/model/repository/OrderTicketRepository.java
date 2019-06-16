5e8d0f4d7fc6920d30d29b75c6ac6c1c798b0105
Hythan <hythan_mco@hotmail.com>
2019-06-16 20:38:12 -0300

    select by show

--- src/main/java/br/edu/utfpr/atividade2/model/repository/OrderTicketRepository.java
+++ src/main/java/br/edu/utfpr/atividade2/model/repository/OrderTicketRepository.java
@@ -1,6 +1,7 @@
 package br.edu.utfpr.atividade2.model.repository;
 
 import br.edu.utfpr.atividade2.model.OrderTicket;
+import br.edu.utfpr.atividade2.model.User;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -16,5 +17,8 @@ public interface OrderTicketRepository extends JpaRepository<OrderTicket, Long>{
     List<OrderTicket> getUserMajorValueDonation(Sort sort);
 
     List<OrderTicket> findByUserId(Long userId);
+
+    @Query("select distinct t.user from OrderTicket t where t.show.id=?1")
+    List<User> getAllUserByShow(Long id);
 
 }
